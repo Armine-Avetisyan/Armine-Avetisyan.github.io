9f428ea7b82bca3fc699383da5ef4a299ad9555a
Armine <armineawtisian@gmail.com>
2021-07-26 16:50:24 +0400

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {AboutPageComponent} from "./pages/about-page/about-page.component";
+import {ContactPageComponent} from "./pages/contact-page/contact-page.component";
+import {GalleryPageComponent} from "./pages/gallery-page/gallery-page.component";
+import {NewsPageComponent} from "./pages/news-page/news-page.component";
+import {ServicePageComponent} from "./pages/service-page/service-page.component";
+import {TrainerPageComponent} from "./pages/trainer-page/trainer-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home',   component: HomePageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'contact', component: ContactPageComponent},
+  {path: 'gallery', component: GalleryPageComponent},
+  {path: 'news', component: NewsPageComponent},
+  {path: 'service', component: ServicePageComponent},
+  {path: 'trainer', component: TrainerPageComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { GalleryPageComponent } from './pages/gallery-page/gallery-page.component';
+import { NewsPageComponent } from './pages/news-page/news-page.component';
+import { ServicePageComponent } from './pages/service-page/service-page.component';
+import { TrainerPageComponent } from './pages/trainer-page/trainer-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    GalleryPageComponent,
+    NewsPageComponent,
+    ServicePageComponent,
+    TrainerPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
