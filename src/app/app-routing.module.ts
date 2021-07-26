50be08f1c9b07f73d63fd4fd88437d92c404c97b
Armine <armineawtisian@gmail.com>
2021-07-26 21:32:18 +0400

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomePageComponent} from "./pages/home-page/home-page.component";
-import {AboutPageComponent} from "./pages/about-page/about-page.component";
-import {ContactPageComponent} from "./pages/contact-page/contact-page.component";
-import {GalleryPageComponent} from "./pages/gallery-page/gallery-page.component";
-import {NewsPageComponent} from "./pages/news-page/news-page.component";
-import {ServicePageComponent} from "./pages/service-page/service-page.component";
-import {TrainerPageComponent} from "./pages/trainer-page/trainer-page.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'home',   component: HomePageComponent},
-  {path: 'about', component: AboutPageComponent},
-  {path: 'contact', component: ContactPageComponent},
-  {path: 'gallery', component: GalleryPageComponent},
-  {path: 'news', component: NewsPageComponent},
-  {path: 'service', component: ServicePageComponent},
-  {path: 'trainer', component: TrainerPageComponent},
+  {path: 'home',loadChildren:() => import('./pages/home-page/home-page.module').then(mod=>mod.HomePageModule)},
+  {path: 'about',loadChildren:() => import('./pages/about-page/about-page.module').then(mod=>mod.AboutPageModule)},
+  {path: 'contact',loadChildren:() => import('./pages/contact-page/contact-page.module').then(mod=>mod.ContactPageModule)},
+  {path: 'gallery',loadChildren:() => import('./pages/gallery-page/gallery-page.module').then(mod=>mod.GalleryPageModule)},
+  {path: 'trainer',loadChildren:() => import('./pages/trainer-page/trainer-page.module').then(mod=>mod.TrainerPageModule)},
+  {path: 'service',loadChildren:() => import('./pages/service-page/service-page.module').then(mod=>mod.ServicePageModule)},
+  {path: 'news',loadChildren:() => import('./pages/news-page/news-page.module').then(mod=>mod.NewsPageModule)},
 
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-import { GalleryPageComponent } from './pages/gallery-page/gallery-page.component';
-import { NewsPageComponent } from './pages/news-page/news-page.component';
-import { ServicePageComponent } from './pages/service-page/service-page.component';
-import { TrainerPageComponent } from './pages/trainer-page/trainer-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    AboutPageComponent,
-    ContactPageComponent,
-    GalleryPageComponent,
-    NewsPageComponent,
-    ServicePageComponent,
-    TrainerPageComponent,
-    HomePageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
